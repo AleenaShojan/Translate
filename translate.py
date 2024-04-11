b03aaa1a2b4672c4234f4e6449ff717ddfa788aa
AleenaShojan <163384538+AleenaShojan@users.noreply.github.com>
2024-04-11 12:50:49 +0530

    translate.py

--- translate.py
+++ translate.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from googletrans import Translator
 
+st.header('Machine Translation Demo')
+input_text = st.text_area("Please enter the text", value='')
+option = st.selectbox(
+    'To which language do you wish to translate this text to?',
+    ('Malayalam', 'Hindi', 'Tamil')
+)
 
-st. header('Machine Translation Demo')
-input = st.text_area("Please enter the text", value = "")
-option = st.selection(
-	'To which language you wish to translate this text to?',
-	('Malayalam', 'Hindi', 'Tamil'))
 if st.button('Translate'):
-	translator = Translator()
-	translation = translator.translate(input, dest = option)
-	st.write(translation.text)
+    translator = Translator()
+    translation = translator.translate(input_text, dest=option.lower())
+    st.write(translation.text)
